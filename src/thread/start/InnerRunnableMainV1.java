45ed7762a082ad9fd03a9a1985aa423799201016
idohyeon <ehgus5026@naver.com>
2025-04-17 20:50:40 +0900

    -정적 중첩 클래스(Runnable 구현체)

---
+++ src/thread/start/InnerRunnableMainV1.java
@@ -0,0 +1,24 @@
+package thread.start;
+
+import util.MyLogger;
+
+public class InnerRunnableMainV1 {
+
+    public static void main(String[] args) {
+        MyLogger.log("main() start");
+
+        MyRunnable runnable = new MyRunnable();
+        Thread thread = new Thread(runnable);
+        thread.start();
+
+        MyLogger.log("main() end");
+    }
+
+    // 정적 중첩 클래스
+    static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            MyLogger.log("run()");
+        }
+    }
+}
