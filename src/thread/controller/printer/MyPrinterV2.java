477dda2c0df8ad4661c31eb5f5cca34e0b2ee675
idohyeon <ehgus5026@naver.com>
2025-04-18 20:29:09 +0900

    -스레드 인터럽트 문제1(인터럽트 메서드)

---
+++ src/thread/controller/printer/MyPrinterV2.java
@@ -0,0 +1,58 @@
+package thread.controller.printer;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class MyPrinterV2 {
+
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Thread printerThread = new Thread(printer, "printer");
+        printerThread.start();
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            log("프린터 할 문서를 입력하세요. 종료 (q): ");
+            String input = sc.nextLine();
+
+            if (input.equals("q")) {
+                printerThread.interrupt();
+                break;
+            }
+            printer.addJob(input);
+        }
+    }
+
+    static class Printer implements Runnable {
+
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>(); // 여러 스레드가 동시에 접근하는 경우 동시성을 지원하는 동시성 컬렉션을 사용해야 함.
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()) {
+                if (jobQueue.isEmpty()) {
+                    continue;
+                }
+
+                try {
+                    String job = jobQueue.poll();
+                    log("출력 시작: " + job + ", 대기 문서: " + jobQueue);
+                    Thread.sleep(3000);
+                    log("출력 완료");
+                } catch (InterruptedException e) {
+                    log("인터럽트!");
+                    break;
+                }
+            }
+            log("프린터 종료");
+        }
+
+        public void addJob(String input) {
+            jobQueue.offer(input);
+        }
+    }
+}
