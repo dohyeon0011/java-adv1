b0491aa17936c25a4dd00fafeaff41b5deb2fd27
idohyeon <ehgus5026@naver.com>
2025-04-19 17:59:55 +0900

    -Volatile

---
+++ src/thread/volatile1/VolatileCountMain.java
@@ -0,0 +1,55 @@
+package thread.volatile1;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+/**
+ * ** 시점의 차이 **
+ *  `main` 스레드가 `flag` 를 `false`로 변경한 시점에 `count` 값은 `1176711196` 이다.
+ *  `work` 스레드가 `flag` 값을 `false`로 확인한 시점에 `count` 값은 `1200000000` 이다.
+ *
+ * 결과적으로 `main` 스레드가 `flag` 값을 `false` 로 변경하고 한참이 지나서야 `work` 스레드는 `flag` 값이 `false`로 변경된 것을 확인한 것이다.
+ */
+public class VolatileCountMain {
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        t.start();
+
+        sleep(1000);
+
+        task.flag = false;  // main 스레드가 flag를 false로 변경하는 시점에 count 값은 `1176711196`이다.
+        log("flag = " + task.flag + ", count = " + task.count + " in main");
+    }
+
+    static class MyTask implements Runnable {
+
+//        boolean flag = true;
+//        long count;
+
+        // `volatile` 을 적용하면 캐시 메모리가 아니라 메인 메모리에 항상 직접 접근하기 때문에 성능이 상대적으로 떨어진다.
+        // `volatile` 이 없을 때: `1176711196` , 약 11억(정확한 숫자는 아니고 대략적인 수치다)
+        // `volatile` 이 있을 때: `222297705` , 약 2.2억
+        // 둘을 비교해보면 물리적으로 약 5배의 성능 차이를 확인할 수 있다. 성능은 환경에 따라 차이가 있다.
+        volatile long count;
+        volatile boolean flag = true;
+
+        @Override
+        public void run() {
+            while (flag) {
+                count++;
+
+                // 1억번에 한 번씩 출력
+                if (count % 100_000_000 == 0) {
+                    // 여기서 정확히 12억에서 변경된 `flag` 값을 읽을 수 있었던 이유는 12억에서 콘솔에 결과를 출력하기 때문이다.
+                    // 콘솔에 결과를 출력하면, 출력하는 동안 스레드가 잠시 대기하며 쉬는데, 이럴 때 컨택스트 스위칭이 발생하면서 캐시 메모리의 값이 갱신된다.
+                    // 참고로 이 부분은 주로 그렇다는 것이지 확실하게 캐시의 갱신을 보장하지는 않는다. 따라서 환경에 따라 결과가 달라질 수 있다.
+                    // 결국 이 상황에서 메모리 가시성 문제를 확실하게 해결하려면 `volatile` 키워드를 사용해야 한다.
+                    log("flag = " + flag + ", count = " + count + " in while()");
+                }
+            }
+            log("flag = " + flag + ", count = " + count + " 종료");   // work 스레드는 이후에 count 값이 1200000000이 되었을 때 flag가 false로 변한 것을 확인할 수 있다.
+        }
+    }
+}
---
+++ src/thread/volatile1/VolatileFlagMain.java
@@ -0,0 +1,36 @@
+package thread.volatile1;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class VolatileFlagMain {
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        log("runFlag = " + task.runFlag);
+        t.start();
+
+        sleep(1000);
+        log("runFlag를 false로 변경 시도");
+        task.runFlag = false;   // main 스레드가 runFlag의 값을 변경해도 CPU 코어1(예시 CPU 코어)이 사용하는 캐시 메모리의 runFlag 값만 false 변경되고, 메인 메모리의 runFlag 값은 true다.
+        log("runFlag = " + task.runFlag);   // CPU는 기본적으로 빠른 로딩을 위해 메인 메모리의 값을 읽어오는 것이 아니라, CPU 코어와 거리로도 성능으로도 가까운 캐시 메모리에서 읽어옴.(캐시 메모리에 언제 반영될 지는 CPU 설계 방식과 실행 환경에 따라 다름. 컨텍스트 스위칭 때 갱신이 됨, 아니면 잠깐 Thread.sleep() 이후에 될 지 말지?)
+        log("main 종료");                     // 이렇게 메인 메모리에 캐시 메모리의 변경된 값이 반영이 되지 않은 상태를 메모리 가시성이라고 함.
+    }
+
+    static class MyTask implements Runnable {
+
+//        boolean runFlag = true;
+        volatile boolean runFlag = true;    // volatile을 사용하면 메인 메모리에 직접 반영하고, 메인 메모리에서 직접 데이터를 조회함.(대신, 캐시 메모리를 사용했을 때 보다는 성능이 떨어짐)
+
+        @Override
+        public void run() {
+            log("task 시작");
+            while (runFlag) {
+                // runFlag가 false로 바뀌면 탈출
+//                System.out.println("hello"); 만약 스레드 실행 도중에 작업을 하게 되면 컨텍스트 스위칭이 발생(출력을 하는 동안 잠깐 대기 상태로 바껴서)해서 메인 메모리의 값이 갱신될 수도 있음.(하지만 100퍼 확실한 경우는 아니라 메모리 가시성이 발생할 확률이 높음)
+            }
+            log("task 종료"); // work 스레드에서 힙 영역의 runFlag 변수가 false가 되면 while문을 빠져나와 이 로그를 출력되길 기대하지만, while문에서 못 빠져 나오고 계속 무한 루프를 돌고 있음.
+        }
+    }
+}
