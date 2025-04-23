58db1e2c01d9d327f154180bc6209db0ae1d0c6e
idohyeon <ehgus5026@naver.com>
2025-04-23 19:50:11 +0900

    -ConcurrentHashMap, ConcurrentSkipListMap

--- src/thread/collection/java/MapMain.java
+++ src/thread/collection/java/MapMain.java
@@ -8,6 +8,8 @@ public class MapMain {
 
     public static void main(String[] args) {
         // 순서 보장 X
+        // synchronized, Lock(ReentrantLock), CAS, 분할 잠금 기술(segment lock)등 다양한 방법을 섞어서 매우 정교한 동기화를 구현하면서 동시에 성능도 최적화.
+        // 각각의 최적화는 매우 어렵게 구현되어 있기 때문에, 자세한 구현을 이해하는 것 보다는, 멀티스레드 환경에 필요한 동시성 컬렉션을 잘 선택해서 사용할 수 있으면 충분하다.
         Map<Integer, String> map1 = new ConcurrentHashMap<>();
         map1.put(3, "data3");
         map1.put(2, "data2");
