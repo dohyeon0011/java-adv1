934464b7b8ea36c604416621766e7434cc42b124
idohyeon <ehgus5026@naver.com>
2025-04-23 21:05:36 +0900

    -ExecutorService 상태 조회 유틸 클래스

---
+++ src/thread/executor/ExecutorUtils.java
@@ -0,0 +1,22 @@
+package thread.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static util.MyLogger.log;
+
+public abstract class ExecutorUtils {
+
+    public static void printState(ExecutorService executorService) {
+        // ExecutorService의 대표 구현체인 ThreadPoolExecutor가 제공하는 기능
+        if (executorService instanceof ThreadPoolExecutor poolExecutor) {
+            int pool = poolExecutor.getPoolSize();  // 스레드 풀에 있는 스레드 개수
+            int active = poolExecutor.getActiveCount(); // 실행중인 스레드(RUNNABLE)
+            int queued = poolExecutor.getQueue().size();    // 큐에 대기중인 작업 숫자
+            long completedTaskCount = poolExecutor.getCompletedTaskCount(); // 스레드가 완료한 작업 개수
+            log("[pool=" + pool + ", active=" + active + ", queuedTasks=" + queued + ", completedTask=" + completedTaskCount + "]");
+        } else {
+            log(executorService);
+        }
+    }
+}
